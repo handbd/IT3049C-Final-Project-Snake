8a7fb446afd72345a25a6220df61412cf1b5e86b
handbrennan <handbd@mail.uc.edu>
2021-12-10 19:42:52 -0500

    drew snake and corrdinates for games

--- snake.js
+++ snake.js
@@ -86,3 +86,171 @@ function pressedKey() {
         changeDirection();
     }
 }
+//change the direction of snake based on arrow key pressed
+function changeDirection() {
+    switch (directionVar) {
+        case "Up":
+            //move "up" only when previous direction is not "down"
+            if (previousDir !== "Down") {
+                direction=directionVar;
+                xSpeed = 0;
+                ySpeed = scale * -speed;
+            } 
+            break;
+
+        case "Down":
+            //move "down" only when previous direction is not "up"
+            if (previousDir !== "Up") {
+                direction=directionVar;
+                xSpeed = 0;
+                ySpeed = scale * speed;
+            } 
+            break;
+
+        case "Left":
+            //move "left" only when previous direction is not "right"
+            if (previousDir !== "Right") {
+                direction=directionVar;
+                xSpeed = scale * -speed;
+                ySpeed = 0;
+            } 
+            break;
+
+        case "Right":
+            //move "right" only when previous direction is not "left"
+            if (previousDir !== "Left") {
+                direction=directionVar;
+                xSpeed = scale * speed;
+                ySpeed = 0;
+            } 
+            break;
+    }
+}
+
+//random coordinates for fruit or virus
+function generateCoordinates() {
+    let xCoordinate = (Math.floor(Math.random() * (max - min) + min)) * scale;
+    let yCoordinate = (Math.floor(Math.random() * (max - min) + min)) * scale;
+    return {xCoordinate, yCoordinate};
+}
+
+//check snake's collision 
+function checkCollision() {
+    let tailCollision=false, virusCollision=false;
+    boundaryCollision=false;
+    //with its own tail
+    for (let i = 0; i < tail.length; i++) {
+        if (snakeHeadX == tail[i].tailX && snakeHeadY == tail[i].tailY) {
+            tailCollision=true;
+        }
+    }
+    //with boundaries
+    if(snakeHeadX >= canvas.width || snakeHeadX < 0 || snakeHeadY >= canvas.height || snakeHeadY < 0)
+    {
+        boundaryCollision=true;
+    }
+    //with virus
+    if(snakeHeadX===virusX && snakeHeadY===virusY) {
+        virusCollision=true;
+    }
+    return (tailCollision || boundaryCollision || virusCollision);
+}
+//-----------------------------------------------------SNAKE-----------------------------------------------------------//
+function drawSnakeHead(color) {
+    context.beginPath();
+    context.arc(snakeHeadX+scale/2, snakeHeadY+scale/2, scale/2, 0, 2 * Math.PI);
+    context.fillStyle = color;
+    context.fill();
+    //eyes
+    context.beginPath();
+    if(direction==="Up") {
+        context.arc(snakeHeadX+(scale/5), snakeHeadY+(scale/5), scale/8, 0, 2 * Math.PI);
+        context.arc(snakeHeadX+scale-(scale/5), snakeHeadY+(scale/5), scale/8, 0, 2 * Math.PI);
+    }
+    else if(direction==="Down") {
+        context.arc(snakeHeadX+(scale/5), snakeHeadY+scale-(scale/5), scale/8, 0, 2 * Math.PI);
+        context.arc(snakeHeadX+scale-(scale/5), snakeHeadY+scale-(scale/5), scale/8, 0, 2 * Math.PI);
+    }
+    else if(direction==="Left") {
+        context.arc(snakeHeadX+(scale/5), snakeHeadY+(scale/5), scale/8, 0, 2 * Math.PI);
+        context.arc(snakeHeadX+(scale/5), snakeHeadY+scale-(scale/5), scale/8, 0, 2 * Math.PI);
+    }
+    else {
+        context.arc(snakeHeadX+scale-(scale/5), snakeHeadY+(scale/5), scale/8, 0, 2 * Math.PI);
+        context.arc(snakeHeadX+scale-(scale/5), snakeHeadY+scale-(scale/5), scale/8, 0, 2 * Math.PI);
+    }
+    context.fillStyle = "black";
+    context.fill();
+}
+
+function drawSnakeTail() {
+let tailRadius = scale/4;
+    for (i = 0; i < tail.length; i++) {
+        tailRadius=tailRadius+((scale/2-scale/4)/tail.length);
+        context.beginPath();
+        context.fillStyle = "#6c2c3a";
+        context.arc((tail[i].tailX+scale/2), (tail[i].tailY+scale/2), tailRadius, 0, 2 * Math.PI);
+        context.fill();
+    }
+}
+
+//shift snake's previous positions to next position
+function moveSnakeForward() {
+tail0=tail[0];
+for (let i = 0; i < tail.length - 1; i++) {
+    tail[i] = tail[i + 1];
+}
+tail[totalTail - 1] = { tailX: snakeHeadX, tailY: snakeHeadY };
+snakeHeadX += xSpeed;
+snakeHeadY += ySpeed;
+}
+
+//only in case of boundary collision
+function moveSnakeBack()
+{
+context.clearRect(0, 0, 500, 500);
+for (let i = tail.length-1; i >= 1; i--) {
+    tail[i] = tail[i - 1];
+}
+if(tail.length>=1) {
+    tail[0] = { tailX: tail0.tailX, tailY: tail0.tailY };
+}
+snakeHeadX -= xSpeed;
+snakeHeadY -= ySpeed;
+drawVirus();
+drawFruit();
+drawSnakeTail();
+}
+
+//display snake
+function drawSnake() {
+drawSnakeHead("#7d4350");
+drawSnakeTail();
+if (checkCollision()) {
+    clearInterval(gameInterval);
+    clearInterval(virusInterval);
+    if(boundaryCollision) {
+        moveSnakeBack();
+    }
+    drawSnakeHead("red");
+    setTimeout(()=>{ 
+        scoreModal.textContent = totalTail;
+        $('#alertModal').modal('show');
+        //if modal is shown, remove the keydown event listener so that snake doesn't move 
+        $( "#alertModal" ).on('shown.bs.modal', function(){
+            window.removeEventListener("keydown", pressedKey);
+        });
+        //when modal hides, reset every variable and add keydown event listener again
+        $('#alertModal').on('hidden.bs.modal', function () {
+            context.clearRect(0, 0, 500, 500);
+            score.innerText = 0;
+            window.addEventListener("keydown", pressedKey);
+            reset();
+          })
+        modalBtn.addEventListener("click", ()=>{
+            context.clearRect(0, 0, 500, 500);
+            score.innerText = 0;
+        });
+    }, 1000);
+}
+}
