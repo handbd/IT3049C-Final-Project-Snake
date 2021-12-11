9d7d9f88246b695718bcf3e712c23e3c1a506251
handbrennan <handbd@mail.uc.edu>
2021-12-10 19:41:44 -0500

    added event listener and play, pause and resume

--- snake.js
+++ snake.js
@@ -20,3 +20,69 @@ var gameInterval,  //interval after which screen will be updated
 var playing, gameStarted;
 var boundaryCollision;
 var tail0;
+startBtn.addEventListener("click", startGame);
+
+//reset the variables to starting value
+function reset() {
+    clearInterval(gameInterval);
+    clearInterval(virusInterval);
+    intervalDuration=150, 
+    minDuration=75;
+    tail = [];
+    totalTail = 0;
+    directionVar = "Right";
+    direction = "Right";
+    previousDir = "Right";
+    xSpeed = scale * speed;
+    ySpeed = 0;
+    snakeHeadX = 0;
+    snakeHeadY = 0;
+    pauseBtn.style.backgroundColor="#fff";
+    resumeBtn.style.backgroundColor="#fff";
+    playing=false, gameStarted=false;
+    boundaryCollision=false;
+}
+
+function startGame() {
+    reset();
+    gameStarted=true;
+    playing=true;
+    fruitPosition();
+    virusPosition();
+    main();
+}
+
+function pauseGame() {
+    window.clearInterval(gameInterval);
+    window.clearInterval(virusInterval);
+    pauseBtn.style.backgroundColor="#ccc";
+    resumeBtn.style.backgroundColor="#fff";
+    playing=false;
+}
+
+function resumeGame()
+{
+    main();
+    pauseBtn.style.backgroundColor="#fff";
+    resumeBtn.style.backgroundColor="#ccc";
+    playing=true;
+}
+
+//EventListener to check which arrow key is pressed
+window.addEventListener("keydown", pressedKey);
+
+function pressedKey() {
+    if(event.keyCode===32 && gameStarted) {
+        if(playing) {
+            pauseGame();
+        }
+        else{
+            resumeGame();
+        }
+    }
+    else {
+        previousDir = direction;
+        directionVar = event.key.replace("Arrow", "");
+        changeDirection();
+    }
+}
